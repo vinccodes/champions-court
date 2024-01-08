eeae357211c92f46899c5039e693cf90521bc804
Vincent Chan <72884143+vinccodes@users.noreply.github.com>
2024-01-08 14:35:42 -0500

    Fix: typo in generate_players_list() in the variable skill_level. Add: sort_players_list() and display_players_list()

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def generate_players_list():
     with open('player-list.txt', 'r') as f:
         # read the file one line at a time
 
-        skill_level = ['A', 'B', 'C]']
+        skill_level = ['A', 'B', 'C']
 
         for line in f:
             # create a {} representing each player
@@ -42,12 +42,58 @@ def check_player_entries(players_list, available_courts):
         return True
 
 
+###
+# Sort the players list by skill level
+###
+def sort_players_list(players_list):
+
+    for i in range(1,len(players_list)):
+        key = players_list[i]
+
+        # move players skill level lower than key to the end of list
+        j = i - 1
+        while j >= 0 and key["skill_level"] < players_list[j]["skill_level"]:
+            players_list[j+1] = players_list[j]
+            j -= 1
+        players_list[j + 1] = key
+    print(players_list)
+    
+
+
+###
+# Assign players to courts based on skill level
+#  Return a list of courts with the assigned players 
+###
+def assign_courts(players_list, available_courts):
+    pass
+    courts_assigned = []
+    # iterate for every available court
+    for i in range(available_courts):
+        # add 4 four players in the court
+        pass
+        # assign all the level A
+
+        # assign all level B
+
+        # assign all level C
+
+###
+# Print out the players_list in human readable format
+###
+def display_player_list(players_list):
+
+    # iterate player list
+    for i in range(len(players_list)):
+        print(f"Player No.{str(i+1)} Name: {players_list[i]['name']} -- Skill Level: {players_list[i]['skill_level']}\n")
+
+
 def main():
     generate_players_list()
     print(players_list)
     
-    check_player_entries(players_list, 5)
-
+    #check_player_entries(players_list, 5)
+    sort_players_list(players_list)
+    display_player_list(players_list)
 
 if __name__ == "__main__":
     main()
